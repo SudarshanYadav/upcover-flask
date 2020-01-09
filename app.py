0200be3cbce6320fedc03ec57d114a4d5baa04d2
McLarenCollege <hello@mclarencollege.in>
2020-01-09 15:42:59 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+import numpy as np
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+
+
+@app.route('/')
+def quote():
+    from sklearn.externals import joblib
+    m = joblib.load('insurance_premium_predictor.pkl')
+    prediction = m.predict([[2163, 22, 100, 100]])
+    return "%d"%prediction
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ prod-Insurance Premium.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Importing Libraries
+
+# In[5]:
+
+
+import numpy as np
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+
+
+# In[4]:
+
+
+from sklearn.externals import joblib
+m = joblib.load('insurance_premium_predictor.pkl')  
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
