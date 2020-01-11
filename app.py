9fe834d789e6caa36a4b732a04f7b868f6c347f3
Sudarshan yadav <sudarshanyadav@Admins-MacBook-Pro.local>
2020-01-11 11:41:13 +0530

    Handle Pre Processing in app.py

--- app.py
+++ app.py
@@ -6,38 +6,36 @@ import numpy as np
 import pandas as pd
 import json
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.externals import joblib
 
+import pre_processing.py
 
-@app.route('/',  methods = ['POST', 'GET'])
-def quote():
-    from sklearn.externals import joblib
-    m = joblib.load('insurance_premium_predictor.pkl')
-    prediction = m.predict([[2163, 22, 100, 100]])
-    response_data = {}
-    response_data['Comprehensive'] = '%f'%prediction
-    response_data['ThirdpartyTheft'] = '%f'%prediction
-    return json.dumps(response_data)
 
 @app.route('/getQuote', methods = ['POST', 'GET'])
 def get_quote():
-    from sklearn.externals import joblib
-    m = joblib.load('insurance_premium_predictor.pkl')
     params = request.form
-    postcode = params.get('postalcode', '2163')
-    year = params.get('year', 1992)
-    day = params.get('day', 6)
-    month = params.get('month', 8)
-    age_at_licence = params.get('ageAtLicence', 21)
-    number_of_claims = params.get('numberOfClaims', 3)
     
-    comprehensive_quote = m.predict([[postcode, 22, age_at_licence, number_of_claims]])
-    theft_quote = m.predict([[postcode, 91, age_at_licence, number_of_claims]])
+    suburb = params.get('suburb', 'Pymble') 
+    number_of_claims = params.get('numberOfClaims', 3)
+    birth_year = params.get('birth_year', 8) 
+    age_at_licence = params.get('ageAtLicence', 21)
+
+    pred_params = proc_param(suburb,number_of_claims,birth_year,age_at_licence)
+    
+    comprehensive_quote = m_comprehensive.predict([pred_params])
+    theft_quote = m_TPT.predict([pred_params])
+    
     response_data = {}
     response_data['Comprehensive'] = '%f'%comprehensive_quote
     response_data['ThirdpartyTheft'] = '%f'%theft_quote
+    
     print(response_data)
     return json.dumps(response_data)
 
 
 if __name__ == '__main__':
+    comprehensive_model = 'models/insurance_comprehensive.pkl'
+    tpt_model = 'models/insurance_tpt.pkl'
+    m_comprehensive = joblib.load(comprehensive_model)
+    m_TPT = joblib.load(comprehensive_model)
     app.run(debug=True, host='0.0.0.0')
